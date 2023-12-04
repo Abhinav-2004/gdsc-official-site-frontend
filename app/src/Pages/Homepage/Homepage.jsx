6f948930c77656cdfa9455bd7b655e3d026faba1
HIITSMANU <132491372+HIITSMANU@users.noreply.github.com>
2023-12-04 20:08:48 +0530

    homepage_updated_with_events

--- app/src/Pages/Homepage/Homepage.jsx
+++ app/src/Pages/Homepage/Homepage.jsx
@@ -40,6 +40,20 @@ export const Homepage = () =>{
           </div>
 
         </div>
+        <a href="/events">
+            <div class="marquee">
+                <p style={{textAlign:"center"}}>
+                  <span class="marq_item">
+                    Events Coming Soon!
+                  </span>
+                  <img src="https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_1080,q_100,w_1080/v1/gcs/platform-data-dsc/events/devfest%20poster.png" alt="gdsc" />
+                  <img src="https://pbs.twimg.com/media/FifX22YVUAAPkTM.jpg" alt="gdsc" />
+                  <img src="https://res.cloudinary.com/startup-grind/image/upload/c_fill,w_500,h_500,g_center/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/events/Orientation-gdsc%20%282%29.png" alt="gdsc" />
+                  <img src="https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_1080,q_100,w_1080/v1/gcs/platform-data-dsc/events/20230922_232642_0000.png" alt="gdsc" />
+                  <img src="https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_1080,q_100,w_1080/v1/gcs/platform-data-dsc/events/launching%20date-01.jpg" alt="gdsc" />
+                </p>
+            </div>
+        </a>
     </div>
     <section class="project_section" id="project">
         <div class="container">
