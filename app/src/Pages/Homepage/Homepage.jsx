3d189c0e15c222af07a12636643d468cfdbf218b
HIITSMANU <132491372+HIITSMANU@users.noreply.github.com>
2023-12-07 23:38:26 +0530

    Update Homepage.jsx

--- app/src/Pages/Homepage/Homepage.jsx
+++ app/src/Pages/Homepage/Homepage.jsx
@@ -89,8 +89,8 @@ export const Homepage = () =>{
     </section>
     <section style={{textAlign:"center",marginTop:"40px"}}>
           <h3>Sponsers</h3>
-          <a href="https://www.coca-colacompany.com/">
-              <img src="https://pbs.twimg.com/profile_images/1615430137926778882/AybyflyO_400x400.jpg" style={{width:"200px"}}/>
+          <a href="https://www.coca-colacompany.com/" style={{textAlign:"center"}}>
+              <img style={{textAlign:"center"}} src="https://pbs.twimg.com/profile_images/1615430137926778882/AybyflyO_400x400.jpg" style={{width:"200px"}}/>
           </a>
     </section>
     <section>
